63bd0b4dbe7c21436cb4fc5e8a372be56e4d1c12
Agencer4382 <asimgencer4382@gmail.com>
2019-06-17 14:29:02 -0400

    TestNGUpdate

---
+++ src/com/Listeners/TestNGListerner.java
@@ -0,0 +1,50 @@
+package com.Listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestNGListerner implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		System.out.println("Test start "+result.getName());
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test success "+result.getName());
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test fail "+result.getName());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("Test fail "+result.getName());
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		
+		
+	}
+
+}
